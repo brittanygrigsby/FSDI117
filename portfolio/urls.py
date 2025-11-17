32bfdf9b85dccb213ce6f39926afbfe5ac727c16
Brittany Grigsby <brittanygrigsby@Mac.lan>
2025-11-16 18:45:32 -0800

    Add full project

---
+++ portfolio/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from portfolio import views
+
+urlpatterns = [
+    path("", views.about_me_view, name="about_me"),
+    path('experience/', views.experience_view, name= "experience"),
+    path('contact/', views.contact_view, name= "contact"),
+
+]
---
+++ projects/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+
+
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='project/')
+    repository = models.URLField()
+    skills = models.ManyToManyField("Skill")
+
+    def __str__(self):
+        return f'{self.name} -- {self.year}'
